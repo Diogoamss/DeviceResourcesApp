76468792285aa0728feff18cb31521567e0929d6
Diogo André Messias <diogoamss@gmail.com>
2026-08-10 18:15:22 -0300

    (W.I.P.)Feat:Mostrar contatos em progresso

--- src/components/ContactsComponent.js
+++ src/components/ContactsComponent.js
@@ -7,7 +7,88 @@ const ContactsComponent = () => {
         const { status } = await Contacts.requestPermissionsAsync()
 
         if(status !== 'granted') {
-            
+            Alert.alert('Permissão Negada', 'Permissão para acessar contatos foi negada.');
+            return;
         }
-    }
-}
+
+        try{
+            const{ data } = await Contacts.getContactsAsync({
+                fields: [Contacts.Fields.Emails, Contacts.Fields.PhoneNumbers],
+            });
+
+            if (data.length > 0 ){
+                setContacts(data);
+            }else {
+                Alert.alert('Sem contatos', 'nunhum contato encontrado.');
+            }
+        } catch (erro) {
+            Alert.alert('Erro', 'Ocorreu um erro ao carregar os contatos.');
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        laodContacts();
+    }, []);
+
+    const renderItem = ({ item }) => (
+        <View style={styles.constactItem}>
+            <Text style={styles.contactName}>
+                {item.firstName} {item.lastName}
+            </Text>
+
+            {item.PhoneNumbers && item.PhoneNumbers.map((phone, index) => (
+                <Text key={index} style={styles.contactDetail}>
+                   {phone.number}
+                </Text>
+            ))}
+
+            {item.emails && item.emails.map((email, index) => (
+                <Text key={index} style={styles.contactDetail}>
+                    {email.email}
+                </Text>
+            ))}
+        </View>
+    );
+
+    return (
+        <View style={styles.container}>
+            <Button title="Recarregar Contatos" onPress={laodContacts} />
+
+            <FlatList 
+                data={Contacts}
+                keyExtractor={(item) => item.id}
+                renderItem={renderItem}
+                contentContainerStyle={styles.list}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#fff',
+
+    },
+    list: {
+        marginTop: 20,
+    },
+    contactItem: {
+        padding: 15,
+        borderBottomWidth: 1,
+        borderColor: #eee,
+    },
+    contactName: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    contactDetail: {
+        fontSize: 14,
+        color: '#555',
+        marginTop: 5,
+    },
+});
+
+export default ContactsComponent;
