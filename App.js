7b9eadfe1144ccb2a7539596cb86022ca01314f5
Diogo André Messias <diogoamss@gmail.com>
2026-08-10 23:08:02 -0300

    Feat: trocando os icones

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaViewBase, Text, View } from 'react-native';
 import React from 'react';
 import ImagePickerComponent from './src/components/ImagePickerComponet';
+import ContactsComponent from './src/components/ContactsComponent';
+import { ScrollView } from 'react-native/types_generated/index';
 
 const App = () => {
   return (
     <SafeAreaView style={style.container }>
       <ImagePickerComponent />
+      <ScrollView>
+        <ContactsComponent />
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- src/components/ContactsComponent.js
+++ src/components/ContactsComponent.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { View, Text, FlatList, Button, Alert, StyleSheet } from "react-native";
 import * as Contacts from 'expo-contacts';
+import { FontAwesome } from "@expo/vector-icons";
 
 const ContactsComponent = () => {
     const laodContacts = async () => {
@@ -37,17 +38,20 @@ const ContactsComponent = () => {
                 {item.firstName} {item.lastName}
             </Text>
 
-            {item.PhoneNumbers && item.PhoneNumbers.map((phone, index) => (
-                <Text key={index} style={styles.contactDetail}>
-                   {phone.number}
-                </Text>
-            ))}
+        {item.phoneNumbers && item.phoneNumbers.map((phone, index) => (
+            <View key={index} style={styles.contactDetailContainer}>
+                <FontAwesome name="phone" size={16} color="#555" style={styles.icon} />
+                <Text style={styles.contactDetail}>{phone.number}</Text>
+            </View>
+        ))}
 
-            {item.emails && item.emails.map((email, index) => (
-                <Text key={index} style={styles.contactDetail}>
-                    {email.email}
-                </Text>
-            ))}
+        {item.emails && item.emails.map((emails, index) => (
+            <View key={index} style={styles.contactDetailContainer}>
+                <FontAwesome name="envelope" size={16} color="#555" style={styles.icon} />
+                <Text style={styles.contactDetail}>{email.email}</Text>
+            </View>
+        ))}
+
         </View>
     );
 
@@ -88,6 +92,14 @@ const styles = StyleSheet.create({
         fontSize: 14,
         color: '#555',
         marginTop: 5,
+    },
+    contactDetailContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 5,
+    },
+    icon: {
+        marginRight: 10,
     },
 });
 
